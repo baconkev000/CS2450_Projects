5083612317e0669418ac8ff8bf21063a5ed85886
baconkev000 <kevinbaconweb@gmail.com>
2024-01-23 14:17:53 -0700

    add project skeleton

---
+++ main.py
@@ -0,0 +1,39 @@
+"""
+Module Docstring
+"""
+
+__author__ = "Kevin Bacon"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+class CPU:
+    def __init__(self):
+        self.accumulator = 0
+        # Other CPU components
+        def execute_READ(self, operand):
+        # Read a word from the keyboard into a specific location in memory
+            pass
+
+        def execute_WRITE(self, operand):
+        # Write a word from a specific location in memory to the screen
+         pass
+
+
+class Register:
+    def __init__(self):
+        self.value = 0
+
+class Memory:
+    def __init__(self, size):
+        self.size = size
+        self.data = [0] * size
+
+
+def main():
+    """ Main entry point of the app """
+    print("hello world")
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
