749d33ace4fc50f9015dc60196029ce01752746b
Matthew Scott <97635556+Mattjsjr@users.noreply.github.com>
2024-02-27 12:55:54 -0700

    Update main.py
    Now resets CPU attributes after an error or end instruction. Also made pointer an attribute of memory instead of CPU, reducing CPU's attributes to 5.

--- main.py
+++ main.py
@@ -26,6 +26,8 @@ class Memory:
         self.data = [None] * size
         #Keeps the users instructions
         self.instructions = []
+        #Used to point to where we are in the program
+        self.pointer = 0
     
     def getMemoryLoc(self, loc):
         return self.data[loc]
@@ -89,11 +91,15 @@ class CPU:
         self.memory = Memory()
         #Window used for GUI
         self.window = Window(self)
-        #Used to point where we are in the program
-        self.pointer = 0
         #List of valid commands
         self.valid_commands = [10, 11, 20, 21, 30, 31, 32, 33, 40, 41, 42, 43]
         self.consoleAcceptingInput = False
+
+    def reset_CPU(self):
+       '''Resets the CPU after an error message or end instruction is hit'''
+       self.memory.pointer = 0
+       self.memory.instructions = []
+       self.memory.clearMemory()
 
     def update_console(self, message):
        '''Updates the message on the console'''
@@ -138,7 +144,7 @@ class CPU:
       '''Processes the user's code and catches an error before running it
       Splits each line of the user's code into two, the command and memory location (command, location)
       Does input validation and turns the command and location into ints
-      If the user's input is not valid, it will print an error message and the line the error is on (self.pointer)
+      If the user's input is not valid, it will print an error message and the line the error is on (self.memory.pointer)
       '''
       #Will be used to store instructions at a certain area in memory starting from 0
       memory_location = 0
@@ -147,7 +153,7 @@ class CPU:
       for code in self.memory.instructions:
 
           #Keeps track of the line number for error handling
-          self.pointer += 1
+          self.memory.pointer += 1
           #Checks if command is END
           if code.upper() == 'END':
              self.memory.data[memory_location] = code
@@ -156,8 +162,9 @@ class CPU:
           
           #Ensures the instruction is at least 4 digits
           if len(code) != 4:
-             error_message = "\nError on line " + str(self.pointer) + ".\nInvalid instruction."
+             error_message = "\nError on line " + str(self.memory.pointer) + ".\nInvalid instruction."
              self.update_console(error_message)
+             self.reset_CPU()
              break
              
           #Chooses which operation is performed
@@ -170,8 +177,9 @@ class CPU:
               location = code[3]
 
             else:
-              error_message = "\nError on line " + str(self.pointer) + ".\nMemory location out of bounds."
+              error_message = "\nError on line " + str(self.memory.pointer) + ".\nMemory location out of bounds."
               self.update_console(error_message)
+              self.reset_CPU()
 
           #Input validation, if the code is not valid, a message is given and the program is exited
           #Ensures input is an int, the command is valid, and the memory location is in bounds
@@ -180,16 +188,19 @@ class CPU:
               command = int(command)
 
           except:
-              error_message = "\nError on line " + str(self.pointer) + ".\nExpected a four digit int."
+              error_message = "\nError on line " + str(self.memory.pointer) + ".\nExpected a four digit int."
               self.update_console(error_message)
+              self.reset_CPU()
               
           if command not in self.valid_commands:
-              error_message = "\nError on line " + str(self.pointer) + ".\nInvalid command."
+              error_message = "\nError on line " + str(self.memory.pointer) + ".\nInvalid command."
               self.update_console(error_message)
+              self.reset_CPU()
 
           elif location < 0 or location > 99:
-              error_message = "\nError on line " + str(self.pointer) + ".\nMemory location out of bounds."
+              error_message = "\nError on line " + str(self.memory.pointer) + ".\nMemory location out of bounds."
               self.update_console(error_message)
+              self.reset_CPU()
 
           else:
              #Code was valid and is added to a location in memory
@@ -205,22 +216,22 @@ class CPU:
       If code is valid, it will execute the appropriate function
       '''
 
-      self.pointer = 0
+      self.memory.pointer = 0
 
       #Processes the code until a halt instruction is reached or a valid instruction is overwritten
       while True:
           
-          code = self.memory.getMemoryLoc(self.pointer)
+          code = self.memory.getMemoryLoc(self.memory.pointer)
           
           #Checks if the memory location has instructions
           if code == None:
              self.update_console("\nError, branched to a location with no defined instruction.")
+             self.reset_CPU()
 
 
           elif code.upper() == 'END':
              #Checks if the code is an end instruction resets the program if it is
-             self.memory.clearMemory()
-             self.pointer = 0
+             self.reset_CPU()
              break
           
           #Chooses which operation is performed
@@ -233,8 +244,9 @@ class CPU:
               location = code[3]
 
             else:
-              error_message = "\nError on line " + str(self.pointer) + ".\nData Overwriting"
+              error_message = "\nError on line " + str(self.memory.pointer) + ".\nData Overwriting"
               self.update_console(error_message)
+              self.reset_CPU()
 
           #Input validation, if the code is not valid, a message is given and the program is exited
           #Ensures input is an int, the command is valid, and the memory location is in bounds
@@ -243,16 +255,19 @@ class CPU:
               command = int(command)
 
           except:
-              error_message = "\nError on line " + str(self.pointer) + ".\nData Overwriting"
+              error_message = "\nError on line " + str(self.memory.pointer) + ".\nData Overwriting"
               self.update_console(error_message)
+              self.reset_CPU()
 
           if command not in self.valid_commands:
-              error_message = "\nError on line " + str(self.pointer) + ".\nData Overwriting"
+              error_message = "\nError on line " + str(self.memory.pointer) + ".\nData Overwriting"
               self.update_console(error_message)
+              self.reset_CPU()
 
           elif location < 0 or location > 99:
-              error_message = "\nError on line " + str(self.pointer) + ".\nData Overwriting"
+              error_message = "\nError on line " + str(self.memory.pointer) + ".\nData Overwriting"
               self.update_console(error_message)
+              self.reset_CPU()
 
           #Instruction at the location is valid and can be processed
           if command == 10:
@@ -271,7 +286,7 @@ class CPU:
                         break
             #Updates the memory on whatever the user entered
             self.memory.updateMemory(location, user_input)
-            self.pointer += 1
+            self.memory.pointer += 1
         
           elif command == 11:
             '''WRITE from memory to console.'''
@@ -281,72 +296,72 @@ class CPU:
             except Exception as inst:
               error_message = "\n" + str(type(inst)) + "\n" + str(inst) + "\nError cannot process None-Type as Instruction"
               self.update_console(error_message)
-            self.pointer += 1
+            self.memory.pointer += 1
           
           elif command == 20:
             '''LOAD value from memory into accumulator'''
             self.accumulator.value = int(self.memory.data[int(location)])
-            self.pointer += 1
+            self.memory.pointer += 1
 
           elif command == 21:
             '''STORE a word from the accumulator into memory'''
             self.memory.data[int(location)] = self.accumulator.value
-            self.pointer += 1
+            self.memory.pointer += 1
 
           elif command == 30:
             # Add a word from a specific memory_location to the accumulator
             self.accumulator.value += int(self.memory.data[location])
-            self.pointer += 1
+            self.memory.pointer += 1
 
           elif command == 31:
             # Subtract a word from a specific memory_location from the word in the accumulator
             self.accumulator.value -= int(self.memory.data[location])
-            self.pointer += 1
+            self.memory.pointer += 1
 
           elif command == 32:
             # Divide the word in the accumulator by a word from a specific memory_location
             self.accumulator.value /= int(self.memory.data[location])
-            self.pointer += 1
+            self.memory.pointer += 1
 
           elif command == 33:
             # Multiply a word from a specific memory_location to the accumulator
             self.accumulator.value *= int(self.memory.data[location])
-            self.pointer += 1
+            self.memory.pointer += 1
 
           elif command == 40:
             #BRANCH to a location in memory
-            if self.pointer != location:#helps to avoid infinite loops
-                self.pointer = location
+            if self.memory.pointer != location:#helps to avoid infinite loops
+                self.memory.pointer = location
             else:
                 self.update_console("\nCannot Branch to Self")
-                self.pointer += 1
+                self.memory.pointer += 1
 
           elif command == 41:
             #BRANCHNEG
             if self.accumulator.value < 0:#checks if accumulator is negative
-                if self.pointer != location:
-                    self.pointer = location
+                if self.memory.pointer != location:
+                    self.memory.pointer = location
                 else:
                     self.update_console("\nCannot Branch to Self")
-                    self.pointer += 1
+                    self.memory.pointer += 1
             else:
-                self.pointer += 1
+                self.memory.pointer += 1
 
           elif command == 42:
             #BRANCHZERO
             if self.accumulator.value == 0:#checks if accumulator is zero
-                if self.pointer != location:
-                    self.pointer = location
+                if self.memory.pointer != location:
+                    self.memory.pointer = location
                 else:
                     self.update_console("\nCannot Branch to Self")
-                    self.pointer += 1
+                    self.memory.pointer += 1
             else:
-                self.pointer += 1
+                self.memory.pointer += 1
 
           elif command == 43:
             #HALT
             simpledialog.askstring("Input", "Paused. Press Ok or cancel to continue")
-            self.pointer +=1
+            self.memory.pointer +=1
 
 def main():
     """ Main entry point of the app """
@@ -359,4 +374,3 @@ def main():
 if __name__ == "__main__":
     """ This is executed when run from the command line """
     main()
-
